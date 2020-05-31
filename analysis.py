511fd0e7ee1612cedabf5af8a873e77ca8c85685
Matt Harrington <harringtonswim@gmail.com>
2020-05-31 18:43:21 -0400

    working query and graphs

--- analysis.py
+++ analysis.py
@@ -9,53 +9,53 @@ parameters = parameters.Parameters() # buffalo
 
 # start by making event histograms
 # go through each specified event
-print ""
-print ""
-print "Graphing event Histograms..."
-for event in parameters.eventHistograms:
-	title = parameters.toTitle(event)
-	database = parameters.databaseFileName
+print()
+print()
+print("Graphing event Histograms...")
+for event in parameters.event_histograms:
+	title = parameters.to_title(event)
+	database = parameters.database_filename
 	process = ["Rscript", "histogram_event.R", event, title, database]
 	subprocess.call(process)
 
-print "done"
-print ""
-print ""
+print("done")
+print()
+print()
 
 # we need the database to get names from here on out
-connection = sqlite3.connect(parameters.databaseFileName)
+connection = sqlite3.connect(parameters.database_filename)
 cursor = connection.cursor()
-print "Graphing team History..."
-for team in parameters.teamsToReview:
+print("Graphing team History...")
+for team in parameters.teams_to_review:
 	# get the team name
 	cursor.execute("select name from Teams where id={}".format(team))
 	teamName = cursor.fetchone()[0]
-	startTimestamp = parameters.convertToTimestamp(parameters.reviewYearStart, parameters.seasonLineMonth, parameters.seasonLineDay)
-	endTimestamp = parameters.convertToTimestamp(parameters.reviewYearEnd, parameters.seasonLineMonth, parameters.seasonLineDay)
+	start_timestamp = parameters.convert_to_timestamp(parameters.review_year_start, parameters.season_line_month, parameters.season_line_day)
+	end_timestamp = parameters.convert_to_timestamp(parameters.review_year_end, parameters.season_line_month, parameters.season_line_day)
 	# run the whole history graphing script
-	process = ["Rscript", "team_history.R", str(team), teamName, str(startTimestamp), str(endTimestamp), database]
+	process = ["Rscript", "team_history.R", str(team), teamName, str(start_timestamp), str(end_timestamp), database]
 	subprocess.call(process)
 	# run each year's meet analysis
-	for simpleYear in range(parameters.reviewYearStart, parameters.reviewYearEnd):
-		startTimestamp = parameters.convertToTimestamp(simpleYear, parameters.seasonLineMonth, parameters.seasonLineDay)
-		endTimestamp = parameters.convertToTimestamp(simpleYear + 1, parameters.seasonLineMonth, parameters.seasonLineDay)
-		title = "{} {}-{} Season".format(teamName, simpleYear, simpleYear + 1)
-		process = ["Rscript", "team_season.R", str(team), title, str(startTimestamp), str(endTimestamp), database]
+	for simple_year in range(parameters.review_year_start, parameters.review_year_end):
+		start_timestamp = parameters.convert_to_timestamp(simple_year, parameters.season_line_month, parameters.season_line_day)
+		end_timestamp = parameters.convert_to_timestamp(simple_year + 1, parameters.season_line_month, parameters.season_line_day)
+		title = "{} {}-{} Season".format(teamName, simple_year, simple_year + 1)
+		process = ["Rscript", "team_season.R", str(team), title, str(start_timestamp), str(end_timestamp), database]
 		subprocess.call(process)
 
-print "done"
-print ""
-print ""
+print("done")
+print()
+print()
 
-print "Graphing Individual Season"
-for swimmer in parameters.swimmersToReview:
+print("Graphing Individual Season")
+for swimmer in parameters.swimmers_to_review:
 	# get the swimmer's name
 	cursor.execute("select name from Swimmers where id={}".format(swimmer))
 	name = cursor.fetchone()[0]
-	for simpleYear in range(parameters.reviewYearStart, parameters.reviewYearEnd):
+	for simple_year in range(parameters.review_year_start, parameters.review_year_end):
 		# graph each year
-		startTimestamp = parameters.convertToTimestamp(simpleYear, parameters.seasonLineMonth, parameters.seasonLineDay)
-		endTimestamp = parameters.convertToTimestamp(simpleYear + 1, parameters.seasonLineMonth, parameters.seasonLineDay)
-		title = "{}'s {}-{} Season".format(name, simpleYear, simpleYear + 1)
-		process = ["Rscript", "individual_season.R", str(swimmer), title, str(startTimestamp), str(endTimestamp), parameters.databaseFileName]
+		start_timestamp = parameters.convert_to_timestamp(simple_year, parameters.season_line_month, parameters.seasonseason_line_dayLineDay)
+		end_timestamp = parameters.convert_to_timestamp(simple_year + 1, parameters.season_line_month, parameters.season_line_day)
+		title = "{}'s {}-{} Season".format(name, simple_year, simple_year + 1)
+		process = ["Rscript", "individual_season.R", str(swimmer), title, str(start_timestamp), str(end_timestamp), parameters.databaseFileName]
 		subprocess.call(process)
